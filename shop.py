5e8f09c02e2f7ac2687f037a22cbcc41117dd831
DemiGodVoid <162964001+DemiGodVoid@users.noreply.github.com>
2025-03-06 15:45:16 -0600

    Update shop.py

--- shop.py
+++ shop.py
@@ -23,7 +23,8 @@ async def shop(ctx):
 
     # Add the !create_role text to the embed
     embed.add_field(name="!create_role", value="Cost: 5k points!", inline=False)
-
+    embed.add_field(name="!group_name", value="Cost: 5k points!", inline=False)
+    
     # Send the embed message to the channel
     await ctx.send(embed=embed)
 
